b4e066099976eb74cf0f10edf4b17ef200bc8880
a.karpenko <a.karpenko@spd-ukraine.com>
2022-12-20 23:34:43 +0200

    add media breakpoints contstants

---
+++ src/ui-constants/media/media.constants.ts
@@ -0,0 +1,13 @@
+export const mediaBreakPointsConstants = {
+  '3xs': 200,
+  '2xs': 360,
+  'xs': 480,
+  'sm': 576,
+  'md': 720,
+  'lg': 1080,
+  'xlg': 1280,
+  '2xlg': 1600,
+  '3xlg': 1920,
+  '4k': 2160,
+  '8k': 4320,
+};
