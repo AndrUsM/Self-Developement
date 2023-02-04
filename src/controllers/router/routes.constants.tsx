1d5f354a3fe9c024f3cbe9997f2e6d27d3b971ee
AndrUsM <oksud22@gmail.com>
2023-02-04 23:58:32 +0200

    update readme file: fixed linter errors, add node version

--- src/controllers/router/routes.constants.tsx
+++ src/controllers/router/routes.constants.tsx
@@ -2,8 +2,8 @@ import React from 'react';
 import { createBrowserRouter } from 'react-router-dom';
 
 export const applicationRoutes = createBrowserRouter([
-    {
-        path: '/',
-        element: <div>Empty Page</div>,
-    }
+  {
+    path: '/',
+    element: <div>Empty Page</div>,
+  }
 ]);
--- src/index.tsx
+++ src/index.tsx
@@ -5,12 +5,12 @@ import ApplicationRouter from './controllers/router';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
-);
+)
 root.render(
   <React.StrictMode>
     <ApplicationRouter></ApplicationRouter>
   </React.StrictMode>
-);
+)
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
